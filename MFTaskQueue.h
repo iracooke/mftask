43a8f328bc2b5a72ee3dc258386a88c97cd03598
Ira Cooke <iracooke@gmail.com>
2010-08-03 22:27:46 +1000

    Added MFTaskQueue. A queue designed to work with MFTask objects

---
+++ MFTaskQueue.h
@@ -0,0 +1,58 @@
+//
+//  MFTaskQueue.h
+//  MFTask
+//
+//  Created by Ira Cooke on 10/05/10.
+//  Copyright 2010 Mudflat Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@class MFTask;
+
+/*! @abstract A simple queue for MFTasks
+
+ MFTaskQueue is designed to fill a similar role to NSOperationQueue, except that it operates on MFTask objects rather than on NSOperations. The reason for this is that it can be tricky wrapping an NSTask in an NSOperation. MFTaskQueue only provides a very simple subset of the functionality of NSOperationQueue. In particular it supports only a simple fifo ordering of task execution.
+ 
+ Note that we use the terminology of tasks here when referring to task methods like terminate, launch etc. But we use the terminology of an OperationQueue where referring to queuing functionality.
+ */
+@interface MFTaskQueue : NSObject {
+	NSMutableArray *tasks;
+	NSInteger maxConcurrentTaskCount;
+	
+
+	// Internal variables for keeping track of the timing of starting tasks. Sometimes tasks need to be staggered.
+	BOOL waitingForTimer;
+	NSTimeInterval staggerSeconds;
+
+}
+
+/*! 
+ @abstract The number of seconds to wait between launching concurrent tasks.  
+ This can be useful for interacting with certain servers, where for example multiple login attempts in succession would be seen as an attack. The default value is 5.0 seconds.
+ */
+@property (nonatomic,assign) NSTimeInterval staggerSeconds;
+
+/*! 
+ @abstract The maximum number of simultaneous tasks to run 
+ */
+@property (nonatomic,assign) NSInteger maxConcurrentTaskCount;
+
+/*! 
+ @abstract Sends a terminate message to all tasks 
+ */
+- (void) terminateAllTasks;
+
+/*! 
+ @abstract Adds a task to the queue, launching it immediately if possible 
+ @param task The MFTask object to add to the queue.
+ */
+- (void) addTask:(MFTask*) task;
+
+/*! 
+ @abstract The number of remaining tasks 
+ @result The number of tasks that are currently running or which have not yet been launched 
+ */
+- (NSInteger) remainingTasks;
+
+@end
---
+++ MFTaskQueue.m
@@ -0,0 +1,138 @@
+//
+//  MFTaskQueue.m
+//  DropSync
+//
+//  Created by Ira Cooke on 10/05/10.
+//  Copyright 2010 Mudflat Software. All rights reserved.
+//
+
+#import "MFTaskQueue.h"
+#import "MFTask.h"
+
+static NSString *const MFTaskQueueKVOObservingContext=@"MFTaskQueueKVOObservingContext";
+
+@implementation MFTaskQueue
+@synthesize maxConcurrentTaskCount,staggerSeconds;
+
+- (id) init {
+	if ( self = [super init]){
+		tasks=[NSMutableArray new];
+		maxConcurrentTaskCount=1;
+		staggerSeconds=5.0;
+		waitingForTimer=NO;
+		return self;
+	} else {
+		return nil;
+	}
+}
+
+- (void) dealloc {
+//	DLog(@"Deallocing taskqueue");
+	for(MFTask *task in tasks){	
+		if ( [task hasLaunched] )
+			[task removeObserver:self forKeyPath:@"isFinished"];
+	}
+	[tasks release];
+	
+	[super dealloc];
+	
+}
+
+
+
+
+//! Send all tasks a terminate message
+- (void) terminateAllTasks {
+	for(MFTask *task in tasks){		
+			[task terminate];
+	}
+	staggerSeconds=0; // Set this so that we cancel things quickly
+}
+
+//! Number of running tasks
+- (NSInteger) numRunning {
+	NSInteger num=0;
+	for(MFTask *task in tasks){
+		if ( [task isRunning] ){
+			num++;
+		}
+	}
+	return num;
+}
+
+//! The number of unfinished tasks. (ie still running or not yet launched) in the queue
+- (NSInteger) remainingTasks {
+	NSInteger num=0;
+	for(MFTask *task in tasks){
+		if ( ![task isFinished] ){
+			num++;
+		} 
+	}
+	return num;
+}
+
+//! If possible launch the next task in the queue. Respects a stagger timer and a maximum for concurrent tasks. 
+- (void) launchTasksIfNeeded {
+	waitingForTimer=NO;
+	NSInteger maxNumToLaunch=[self maxConcurrentTaskCount]-[self numRunning];
+	
+	for(MFTask *task in tasks){
+		
+		if ( ![task hasLaunched] && (maxNumToLaunch>0) ){
+				//	DLog(@"Starting task %@",[[task taskLog] fromPath]);
+			[task addObserver:self forKeyPath:@"isFinished" options:0 context:MFTaskQueueKVOObservingContext];
+			[task launch];
+			maxNumToLaunch--;
+			if ( maxNumToLaunch > 0 ){
+					//				DLog(@"Launching timer");
+					// We still need to start more but we need to wait a bit or ssh will lock us out
+				[NSTimer scheduledTimerWithTimeInterval:staggerSeconds target:self selector:@selector(launchTasksIfNeeded) userInfo:nil repeats:NO];
+				waitingForTimer=YES;
+				return;
+			}
+		}
+	}
+	
+}
+
+//! Add a task to the queue. Tasks will be launched in the order they are added. If possible this method will immediately launch the task.
+- (void) addTask:(MFTask*) task {
+	[tasks addObject:task];
+	if ( !waitingForTimer ){
+		[self launchTasksIfNeeded];
+	}
+}
+
+
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change 
+					  context:(void *)context {
+    if ( (context==MFTaskQueueKVOObservingContext) && [keyPath isEqualToString:@"isFinished"] ) {
+		
+		// If the task isFinished then we should remove it from the queue and launch another if possible
+		if ( [object isFinished] ){
+			
+			// Only if the task was launched will we have registered 
+			[object removeObserver:self forKeyPath:@"isFinished"];
+			
+			// Remove the task from the queue 
+			[tasks removeObject:object];	
+			
+			
+			if ( !waitingForTimer ){
+				
+				[self launchTasksIfNeeded];
+				
+			} else {
+			//	DLog(@"Waiting for timer");
+			}
+		} else {
+			// Should never get here
+			ALog(@"Warning: isFinished was unexpectedly set to NO after task initialization");
+			
+		}
+    } else {
+		[super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+	}
+}
+
+@end
